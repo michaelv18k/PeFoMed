2f08042e81a56f84c814ae33788a8ce83fd7fc61
Michael <vmichael3690@gmail.com>
2025-05-22 12:43:54 +0530

    update

--- pefomed/tasks/__init__.py
+++ pefomed/tasks/__init__.py
@@ -6,18 +6,19 @@
 """
 from pefomed.common.registry import registry
 from pefomed.tasks.base_task import BaseTask
-from pefomed.tasks.captioning import CaptionTask
-from pefomed.tasks.image_text_pretrain import ImageTextPretrainTask
-from pefomed.tasks.multimodal_classification import (
-    MultimodalClassificationTask,
-)
-from pefomed.tasks.retrieval import RetrievalTask
-from pefomed.tasks.vqa import VQATask, GQATask, AOKVQATask, DisCRNTask
-from pefomed.tasks.vqa_reading_comprehension import VQARCTask, GQARCTask
-from pefomed.tasks.dialogue import DialogueTask
-from pefomed.tasks.text_to_image_generation import TextToImageGenerationTask
-from pefomed.tasks.vg import MEDVGTask
-from pefomed.tasks.vim_mamba import mambaVQATask
+# from pefomed.tasks.captioning import CaptionTask
+# from pefomed.tasks.image_text_pretrain import ImageTextPretrainTask
+# from pefomed.tasks.multimodal_classification import (
+#     MultimodalClassificationTask,
+# )
+# from pefomed.tasks.retrieval import RetrievalTask
+from pefomed.tasks.vqa import VQATask
+# from pefomed.tasks.vqa import VQATask, GQATask, AOKVQATask, DisCRNTask
+# from pefomed.tasks.vqa_reading_comprehension import VQARCTask, GQARCTask
+# from pefomed.tasks.dialogue import DialogueTask
+# from pefomed.tasks.text_to_image_generation import TextToImageGenerationTask
+# from pefomed.tasks.vg import MEDVGTask
+# from pefomed.tasks.vim_mamba import mambaVQATask
 
 def setup_task(cfg):
     assert "task" in cfg.run_cfg, "Task name must be provided."
