a479ae21b69a0550896f1c20d0f428a9d5b123cc
Michael <vmichael3690@gmail.com>
2025-05-24 23:33:21 +0530

    update

--- pefomed/models/pefomed/model_base.py
+++ pefomed/models/pefomed/model_base.py
@@ -551,11 +551,9 @@ class ModelBase(BaseModel):
 
         # Load tokenizer (use local path if available)
         tokenizer_path = llama_model_path if os.path.exists(llama_model_path) else "meta-llama/Llama-2-7b-chat-hf"
-        # llama_tokenizer = LlamaTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", use_fast=False)
-        # llama_tokenizer.pad_token = "$$"
-        # tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
-        tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-alpha")
-        tokenizer.pad_token = tokenizer.eos_token
+        llama_tokenizer = LlamaTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-alpha", use_fast=False)
+        llama_tokenizer.pad_token = "$$"
+
 
         if low_resource:
             # Load quantized model (4-bit) from local path or Hugging Face
@@ -568,7 +566,6 @@ class ModelBase(BaseModel):
                 bnb_4bit_compute_dtype=torch.float16
             )
             llama_model = AutoModelForCausalLM.from_pretrained(
-                # "mistralai/Mistral-7B-v0.1",
                 "HuggingFaceH4/zephyr-7b-alpha",
                 quantization_config=quant_config,
                 device_map={"": low_res_device}
